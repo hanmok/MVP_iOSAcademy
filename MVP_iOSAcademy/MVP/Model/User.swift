27ed2fe0d018d63787c17df48fd473f47e38beaa
hanmok <dmammmm@naver.com>
2021-10-22 23:48:46 +0900

    initial commit

---
+++ MVP_iOSAcademy/MVP/Model/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  MVP_iOSAcademy
+//
+//  Created by Mac mini on 2021/10/21.
+//
+
+import Foundation
+
+struct User: Codable {
+    let name: String
+    let email: String
+    let username: String
+}
